0f02c31445d803655fbb07aa2694544bae42311b
Kate Torz <ktorz@wp.pl>
2018-04-21 09:08:55 +0200

    create container

---
+++ my-react-app/src/routes/home/home.component.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import {Container} from './home.style';
+
+class Home extends Component{
+    render(){
+        return(
+            <Container>
+                home
+            </Container>
+        );
+    }
+}
+export default Home;
